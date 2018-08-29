977d235191a71caae1746c0e7032027774f5c42d
diegodsgarcia <diego.ds.garcia@gmail.com>
2018-08-29 14:35:13 -0300

    Finish example of cloud vision API

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { Http, Headers, RequestOptions} from '@angular/http';
+
+import { LoadingController, Loading } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +12,74 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  imagem: string;
+  loading: Loading;
+  resultados: object[];
+
+
+  constructor(
+    private change: ChangeDetectorRef,
+    private loadingCtrl: LoadingController,
+    private camera: Camera, 
+    private http: Http 
+    ) {}
+
+  tirarFoto() {
+    this.loading = this.loadingCtrl.create({
+      content: 'Aguarde...'
+    });
+    this.loading.present();
+
+    const options: CameraOptions = {
+      quality: 50,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options)
+    .then(imagemDados => {
+      this.imagem = 'data:image/jpeg;base64,' + imagemDados;
+      return imagemDados;
+    })
+    .then(imagem => {
+      return this.requisicaoParaAPI(imagem);
+    })
+    .then(resultado => {
+      this.resultados = resultado.labelAnnotations;
+      this.loading.dismiss();
+      this.change.detectChanges();
+    })
 
   }
 
+  requisicaoParaAPI(imagemBase64) {
+    const apiKey = '';
+    const url = `https://vision.googleapis.com/v1/images:annotate?key=${apiKey}`;
+    const request = {
+      "requests":[
+        {
+          "image":{
+            "content": imagemBase64
+          },
+          "features":[
+            {
+              "type":"LABEL_DETECTION",
+              "maxResults": 1
+            }
+          ]
+        }
+      ]
+    }
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+    const options = new RequestOptions({ headers: headers});
+
+    return this.http.post(url, request, options)
+      .toPromise()
+      .then(res => res.json().responses[0])
+  }
+
+  verificarPrecisao(valor) {
+    return Math.round(valor * 100);
+  }
 }
